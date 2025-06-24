d730fdaa1187f2b185090287f80195ebeccb23d7
Anilkalbi <141136121+Anilkalbi@users.noreply.github.com>
2025-06-24 23:49:17 +0530

    Update DBConnection.java

--- foodapp-web-application-main/DBConnection.java
+++ foodapp-web-application-main/DBConnection.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 public class DBConnection {
 	private static final String  url="jdbc:mysql://localhost:3306/foodApp";
 	private static final String userName="root";
-	private static final String password="2003";
+	private static final String password="Kalbi@7975";
 	public static Connection connection;
 
 	public  static Connection getConnection() {
